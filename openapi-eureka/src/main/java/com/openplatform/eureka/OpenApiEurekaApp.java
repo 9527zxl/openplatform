b8eb73ccd7d319756a351a44e90b9cc41ae2a4ac
9527zxl <1127580235@qq.com>
2021-11-30 16:16:19 +0800

    Eureka搭建

---
+++ openapi-eureka/src/main/java/com/openplatform/eureka/OpenApiEurekaApp.java
@@ -0,0 +1,13 @@
+package com.openplatform.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class OpenApiEurekaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiEurekaApp.class,args);
+    }
+}
