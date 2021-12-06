de521b24d60cd2086fdd76ffcf6687eb8d492790
9527zxl <1127580235@qq.com>
2021-12-06 16:37:01 +0800

    Redis设置

--- openapi-cache/src/main/java/com/openplatform/cache/config/CacheConfig.java
+++ openapi-cache/src/main/java/com/openplatform/cache/config/CacheConfig.java
@@ -1,9 +1,21 @@
 package com.openplatform.cache.config;
 
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
 
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
+
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object o, Method method, Object... objects) {
+                return null;
+            }
+        };
+    }
 
 }
